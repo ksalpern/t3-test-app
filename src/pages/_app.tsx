df011766bea825428bb6ccc4e6fcc6ab8c74b140
Kseniia Pidopryhora <ksenia.pidopryhora@gmail.com>
2023-07-14 16:11:03 +0300

    update title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
       <Head>
-        <title>Chirp</title>
+        <title>T3 test</title>
         <meta name="description" content="💭" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
